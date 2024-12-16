3568cc693c1f696740164447746e174ff64e94a1
unknown <hellois.bernard@etu.univ-grenoble-alpes.fr>
2024-12-16 14:50:16 +0100

    Style

--- WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4015380432b5f8b56439795b5fa08a6e8020ebb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425376f461f75fa15425e97b2437c134e4e5068e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
