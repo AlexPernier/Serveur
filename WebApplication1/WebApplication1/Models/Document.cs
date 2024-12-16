af72d31807d8669cc73d21b81e339139d07cd37f
AlexPernier <53002065+AlexPernier@users.noreply.github.com>
2024-12-16 14:57:15 +0100

    Add files via upload

---
+++ WebApplication1/WebApplication1/Models/Document.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SiteWebMultiSport.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FileName { get; set; } // Nom du fichier
+
+        [Required]
+        public byte[] Content { get; set; } // Contenu du fichier (stocké en base)
+
+        public string ContentType { get; set; } // Type MIME, ex: "application/pdf"
+
+        public DateTime UploadDate { get; set; } = DateTime.Now;
+
+        public bool IsValidated { get; set; } = false; // Statut de validation
+
+        public int AdherantId { get; set; }
+        [ForeignKey("AdherantId")]
+        public Adherant Adherant { get; set; } // Relation avec l'adhérent
+    }
+}
